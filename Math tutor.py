0ba95d769e499be56aa8f0954aa90a955a08ca96
phoebewychan <phoebe_smy@hotmail.com>
2025-05-15 21:34:58 +0100

    Update Math tutor.py
    An if statement is added on line 29. 'Continuing to next question' will not print at the last question.

--- Math tutor.py
+++ Math tutor.py
@@ -7,7 +7,7 @@ def math_tutor():
             if numofq > 0:
                 break
             else: 
-                print ('Number of questions must be greater than zero.')
+                print('Number of questions must be greater than zero.')
         else:
             print('Invalid input. Please enter a number.')
     while True:
@@ -19,14 +19,15 @@ def math_tutor():
             print('Invalid input. Please enter a number.')
     score = 0
     question_list = []
-    for i in range (numofq):
+    for _ in range(numofq):
         a = random.randrange(1,max_val+1)
         b = random.randrange(1,max_val+1)
         try:
             answer = int(input(f'{a} x {b} = '))
         except ValueError:
              print ('Invalid input. Please enter a number for your answer.')
-             print('Continuing to the next question.')
+             if _ < numofq - 1: 
+                 print('Continuing to the next question.')
              continue
         correct_answer = a * b
         question_list.append(f'{a} x {b} = {correct_answer}')
@@ -34,7 +35,6 @@ def math_tutor():
             score += 1
     percentscore = round((score / numofq) * 100)
     question_list = ', '.join(question_list)
-    print(question_list)
 
     return (f'Thank you for playing. You have got {score} out of {numofq} questions correct. Your score is {percentscore}%. Here are all the questions with the correct answer: {question_list}')
 
